4e89392bd0c983cb31b003b7e64fcf1a9342839e
sqx101 <145257477+sqx101@users.noreply.github.com>
2024-02-09 06:02:51 -0600

    init

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,17 @@
-function main() {
-  console.log("hello world");
-}
-main();
+import { serve } from '@hono/node-server';
+import { Hono } from 'hono';
+
+const app = new Hono();
+
+app.get('/', (c) => c.html(
+  `<!DOCTYPE html>
+  <html>
+    <head>
+      <meta property="fc:frame" content="vNext" />
+      <meta property="fc:frame:image" content="https://i.postimg.cc/ry0c7285/Nouns-en-MAXPAIN.png" />
+    </head>
+  </html>`
+));
+console.log ('server is running')
+
+serve(app);
