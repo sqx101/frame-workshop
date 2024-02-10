72bb2882b4c57f229801c0c90903bd8e76c377a7
sqx101 <145257477+sqx101@users.noreply.github.com>
2024-02-10 05:27:04 -0600

    second frame debug

--- src/main.ts
+++ src/main.ts
@@ -20,19 +20,22 @@ app.get('/', (c) => c.text(
       <meta property="fc:frame:button:1target" content="https://frame-workshop-production-d62f.up.railway.app/button1" />
     </head>
   </html>`
-));
+))
 
-app.post("/button1", (c) => c.text(
-  '<!DOCTYPE html>
-  <html>
-    <head>
-      <meta property="og:title" content="Nouns en The Spiral" />
-      <meta property="og:image" content="https://i.postimg.cc/2SdKg2bQ/Nouns-en-Pho.png" />
-      <meta property="fc:frame" content="vNext" />
-      <meta property="fc:frame:image" content="https://i.postimg.cc/2SdKg2bQ/Nouns-en-Pho.png" />
+app.post("/button1", (c) => {
+ // https://i.postimg.cc/2SdKg2bQ/Nouns-en-Pho.png
+ c.text(`
+    <!DOCTYPE html>
+    <html>
+     <head>
+        <meta property="og:title" content="Nouns en The Spiral" />
+        <meta property="og:image" content="https://i.postimg.cc/2SdKg2bQ/Nouns-en-Pho.png" />
+        <meta property="fc:frame" content="vNext" />
+        <meta property="fc:frame:image" content="https://i.postimg.cc/2SdKg2bQ/Nouns-en-Pho.png" />
     </head>
-  </html>'
-));
+  </html>
+  `)
+});
 
 console.log('server is running')
 serve({
