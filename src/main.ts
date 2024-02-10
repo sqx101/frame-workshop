fe4416bfef68a7d397cfb57ecf639d331cdbe850
sqx101 <145257477+sqx101@users.noreply.github.com>
2024-02-10 04:35:01 -0600

    add buttons

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,13 @@ app.get('/', (c) => c.text(
   `<!DOCTYPE html>
   <html>
     <head>
-      <meta property="og:title" content= "Nouns en Max Pain" >
       <meta property="og:image" content="https://i.postimg.cc/ry0c7285/Nouns-en-MAXPAIN.png" />
       <meta property="fc:frame" content="vNext" />
       <meta property="fc:frame:image" content="https://i.postimg.cc/ry0c7285/Nouns-en-MAXPAIN.png" />
+      <meta property="fc:frame:button:1" content="Green" />
+      <meta property="fc:frame:button:2" content="Purple" />
+      <meta property="fc:frame:button:3" content="Red" />
+      <meta property="fc:frame:button:4" content="Blue" />
     </head>
   </html>`
 ));
