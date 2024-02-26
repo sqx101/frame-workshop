f5ab858f462d754c4e837f1910f64d7ff660163d
sqx101 <145257477+sqx101@users.noreply.github.com>
2024-02-26 12:59:24 -0600

    removed comments

--- src/main.tsx
+++ src/main.tsx
@@ -12,11 +12,7 @@ app.get('/', (c) => {
         <meta property="fc:frame" content="vNext" />
         <meta property="fc:frame:image" content="https://i.postimg.cc/ry0c7285/Nouns-en-MAXPAIN.png" />
         <meta property="fc:frame:button:1" content="⌐🆇-🆇" />
-        {/* The following meta tags are commented out
-        <meta property="fc:frame:button:2" content="Purple" />
-        <meta property="fc:frame:button:3" content="Red" />
-        <meta property="fc:frame:button:4" content="Blue" />
-        */}
+
         <meta property="fc:frame:button:1:action" content="post" />
         <meta property="fc:frame:button:1target" content="https://frame-workshop-production-d62f.up.railway.app/button1" />
       </head>
