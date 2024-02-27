52d298bb58dc496b3dac3f123bf5d30fe9c1b2ae
sqx101 <145257477+sqx101@users.noreply.github.com>
2024-02-27 05:36:58 -0600

    fall back to video

--- src/main.tsx
+++ src/main.tsx
@@ -1,73 +1,16 @@
 /*
-* Adding interactivity
-*
-* - Add post route handler as defined in the layout
-* - Warpcast will send a object in this format:
-* {
-    "untrustedData": {
-      "fid": 2,
-      "url": "https://fcpolls.com/polls/1",
-      "messageHash": "0xd2b1ddc6c88e865a33cb1a565e0058d757042974",
-      "timestamp": 1706243218,
-      "network": 1,
-      "buttonIndex": 2,
-      "inputText": "hello world", // "" if requested and no input, undefined if input not requested
-      "castId": {
-        "fid": 226,
-        "hash": "0xa48dd46161d8e57725f5e26e34ec19c13ff7f3b9"
-      }
-    },
-    "trustedData": {
-      "messageBytes": "d2b1ddc6c88e865a33cb1a565e0058d757042974..."
-    }
-  }
-
-*
-* We compose our dynamic logic around which buttonIndex is sent back
-* and render a different frame depending on which button index is send
-*
-* Since the data is being send over the wire, we are checking that it does
-* in fact exist and if not, throwing an error
-*
-*/
+ * - Some config updates to allow us to use jsx,
+ * - Using c.render now instead of c.text
+ * - Set up the layout to be more HTML like (jsx) also intro to props.
+ * - Use props to set the base frame image url and the post url
+ */
 
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
-import { HTTPException } from "hono/http-exception";
-
-type FrameDataRes = {
-  fid: number;
-  url: string;
-  messageHash: string;
-  timestamp: number;
-  network: number;
-  buttonIndex: 1 | 2 | 3 | 4;
-  castId: {
-    fid: number;
-    hash: string;
-  };
-};
 
 const app = new Hono();
 
-const ButtonRenderer = ({ buttonData }: { buttonData: string[] }) => (
-  <>
-    {buttonData.map((content, idx) => (
-      <meta property={`fc:frame:button:${idx + 1}`} content={`${content}`} />
-    ))}
-  </>
-);
-
-const Layout = ({
-  imgUrl,
-  postUrl,
-  buttonData,
-}: {
-  imgUrl: string;
-  postUrl?: string;
-  buttonData?: string[];
-}) => {
-  const defaultButtonData = ["Btn 1", "Btn 2", "Btn 3", "Btn 4"];
+const Layout = ({ imgUrl, postUrl }: { imgUrl: string; postUrl: string }) => {
   return (
     <html>
       <head>
@@ -75,11 +18,7 @@ const Layout = ({
         <meta property="og:image" content={`${imgUrl}`} />
         <meta property="fc:frame" content="vNext" />
         <meta property="fc:frame:image" content={`${imgUrl}`} />
-        <meta
-          property="fc:frame:post_url"
-          content={postUrl || "https://frame-workshop.up.railway.app/res"}
-        />
-        {<ButtonRenderer buttonData={buttonData || defaultButtonData} />}
+        <meta property="fc:frame:post_url" content={postUrl} />
       </head>
     </html>
   );
@@ -88,66 +27,14 @@ const Layout = ({
 app.get("/", (c) =>
   c.render(
     <Layout
-      imgUrl="https://i.postimg.cc/Vs7LmQ6K/toshi-logo.png"
+      imgUrl="https://i.imgur.com/sS717ci.jpg"
       postUrl="https://frame-workshop.up.railway.app/res"
     />,
   ),
 );
 
-app.post("/res", async (c) => {
-  const frameData: { untrustedData: FrameDataRes } = await c.req.json();
-
-  console.log(frameData);
-
-  if (!frameData?.untrustedData?.buttonIndex) {
-    throw new HTTPException(400, { message: "frame data missing" });
-  }
-
-  const { buttonIndex } = frameData.untrustedData;
-
-  // This URL will be dynamically determined based on the button clicked
-  let imageUrl;
-
-  switch (buttonIndex) {
-    case 1:
-      imageUrl = "https://i.postimg.cc/kXzJRQW8/Brian-and-Toshi.png";
-      break;
-    case 2:
-      imageUrl = "https://i.postimg.cc/BnFqTjCz/toshi-the-cat.png";
-      break;
-    case 3:
-      imageUrl = "https://i.postimg.cc/kXBM51Rq/nft-toshi.png";
-      break;
-    case 4:
-      imageUrl = "https://i.postimg.cc/0Nq9c9Wy/Toshi-Tools.jpg";
-      break;
-    default:
-      imageUrl = "https://i.postimg.cc/QMkhzvwk/Toshi-Coming-Soon.png";
-      break;
-  }
-
-  // Constructing the HTML string with the dynamic image URL
-  const htmlResponse = `
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <meta property="fc:frame" content="vNext" />
-        <meta property="fc:frame:image" content="${imageUrl}" />
-        <meta property="og:image" content="${imageUrl}" />
-      </head>
-    </html>
-  `;
-
-  // Set headers to prevent caching
-  c.res.headers.set('Cache-Control', 'no-cache, no-store, must-revalidate');
-  c.res.headers.set('Pragma', 'no-cache');
-  c.res.headers.set('Expires', '0');
-
-  // Sending the response
-  return c.html(htmlResponse, 200);
-});
-
 console.log("sever is running");
+
 serve({
   fetch: app.fetch,
   port: Number(process.env.PORT) || 3000,
